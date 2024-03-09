b99c613e3fdad8649913a18e22f45143f18b6294
EthanPeddie <kaungmyatthu2620@gmail.com>
2024-03-09 12:04:55 +0630

    Complete Setting Up the SplashScreen and Custom Font

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from "expo-status-bar";
+import { useCallback, useEffect, useState } from "react";
 import { StyleSheet, Text } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import * as SplashScreen from "expo-splash-screen";
-import { useCallback, useEffect, useState } from "react";
+import * as Font from "expo-font";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -10,9 +11,32 @@ export default function App() {
   const [appIsReady, setAppIsReady] = useState(false);
 
   useEffect(() => {
-    setTimeout(() => {
-      setAppIsReady(true);
-    }, 2000);
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          black: require("./assets/fonts/Roboto-Black.ttf"),
+          blackItalic: require("./assets/fonts/Roboto-BlackItalic.ttf"),
+          bold: require("./assets/fonts/Roboto-Bold.ttf"),
+          boldItalic: require("./assets/fonts/Roboto-BoldItalic.ttf"),
+          italic: require("./assets/fonts/Roboto-Italic.ttf"),
+          light: require("./assets/fonts/Roboto-Light.ttf"),
+          lightItalic: require("./assets/fonts/Roboto-LightItalic.ttf"),
+          medium: require("./assets/fonts/Roboto-Medium.ttf"),
+          mediumItalic: require("./assets/fonts/Roboto-MediumItalic.ttf"),
+          regular: require("./assets/fonts/Roboto-Regular.ttf"),
+          thin: require("./assets/fonts/Roboto-Thin.ttf"),
+          thinItalic: require("./assets/fonts/Roboto-ThinItalic.ttf"),
+        });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setTimeout(() => {
+          setAppIsReady(true);
+        }, 2000);
+      }
+    }
+
+    prepare();
   }, []);
 
   const onLayout = useCallback(async () => {
@@ -26,9 +50,7 @@ export default function App() {
   return (
     <SafeAreaProvider style={styles.container} onLayout={onLayout}>
       <SafeAreaView>
-        <Text style={styles.label}>
-          Open up App.js to start working on your app!
-        </Text>
+        <Text style={styles.label}>Hello World</Text>
         <StatusBar style="auto" />
       </SafeAreaView>
     </SafeAreaProvider>
@@ -45,5 +67,6 @@ const styles = StyleSheet.create({
   label: {
     fontSize: 18,
     color: "black",
+    fontFamily: "black",
   },
 });
