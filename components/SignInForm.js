16dc6b99ffe3424b9eb82e43b94d36632611597d
EthanPeddie <kaungmyatthu2620@gmail.com>
2024-03-13 10:51:38 +0630

    Adding the form image

--- components/SignInForm.js
+++ components/SignInForm.js
@@ -1,13 +1,17 @@
-import { StyleSheet } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 import React from "react";
 import { Feather } from "@expo/vector-icons";
 
 import Input from "./Input";
 import SubmitButton from "./SubmitButton";
+import Logo from "../assets/images/logo.png";
 
 const SignUpForm = () => {
   return (
     <>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={Logo} resizeMode="contain" />
+      </View>
       <Input label="Email" icon="mail" iconPack={Feather} />
       <Input label="Password" icon="lock" iconPack={Feather} />
       <SubmitButton
@@ -21,4 +25,12 @@ const SignUpForm = () => {
 
 export default SignUpForm;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imageContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: "50%",
+  },
+});
