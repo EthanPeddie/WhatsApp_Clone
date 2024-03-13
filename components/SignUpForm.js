729d77420444797af5eb5122df4d4e53c8836033
EthanPeddie <kaungmyatthu2620@gmail.com>
2024-03-13 10:59:30 +0630

    Fix the hiding keyboard on Android and iOS for SignUpForm

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -1,4 +1,11 @@
-import { Image, StyleSheet, View } from "react-native";
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  View,
+} from "react-native";
 import React from "react";
 import { FontAwesome, Feather } from "@expo/vector-icons";
 
@@ -9,23 +16,30 @@ import Logo from "../assets/images/logo.png";
 const SignUpForm = () => {
   return (
     <>
-      <View style={styles.imageContainer}>
-        <Image style={styles.image} source={Logo} resizeMode="contain" />
-      </View>
-      <Input
-        label="First Name"
-        icon="user-o"
-        iconPack={FontAwesome}
-        errorMessage="required"
-      />
-      <Input label="Last Name" icon="user-o" iconPack={FontAwesome} />
-      <Input label="Email" icon="mail" iconPack={Feather} />
-      <Input label="Password" icon="lock" iconPack={Feather} />
-      <SubmitButton
-        disable={false}
-        title="Sign Up"
-        onPress={() => console.log("Sign Up")}
-      />
+      <ScrollView>
+        <KeyboardAvoidingView
+          behavior={Platform.OS == "ios" ? "height" : undefined}
+          keyboardVerticalOffset={100}
+        >
+          <View style={styles.imageContainer}>
+            <Image style={styles.image} source={Logo} resizeMode="contain" />
+          </View>
+          <Input
+            label="First Name"
+            icon="user-o"
+            iconPack={FontAwesome}
+            errorMessage="required"
+          />
+          <Input label="Last Name" icon="user-o" iconPack={FontAwesome} />
+          <Input label="Email" icon="mail" iconPack={Feather} />
+          <Input label="Password" icon="lock" iconPack={Feather} />
+          <SubmitButton
+            disable={false}
+            title="Sign Up"
+            onPress={() => console.log("Sign Up")}
+          />
+        </KeyboardAvoidingView>
+      </ScrollView>
     </>
   );
 };
