2c445ab03d418fd617b5aa533a98fd92f5df62ac
EthanPeddie <kaungmyatthu2620@gmail.com>
2024-03-17 10:51:34 +0630

    Going to the main page when user sign up

--- navigations/AppNavigator.js
+++ navigations/AppNavigator.js
@@ -3,9 +3,12 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import MainNavigator from "./MainNavigator";
 import AuthScreen from "../screens/AuthScreen";
+import { useSelector } from "react-redux";
 
 const AppNavigator = () => {
-  const isAuth = false;
+  const isAuth = useSelector(
+    (state) => state.auth.token !== null && state.auth.token !== ""
+  );
   return (
     <NavigationContainer>
       {isAuth && <MainNavigator />}
--- utils/actions/AuthAction.js
+++ utils/actions/AuthAction.js
@@ -23,19 +23,10 @@ const createUser = async (firstName, lastName, email, userId) => {
 export const SignUp = (values) => async (dispatch) => {
   const { firstName, lastName, email, password } = values;
   const auth = getAuth(app);
-  try {
-    const res = await createUserWithEmailAndPassword(auth, email, password);
-    const { uid, stsTokenManager } = res.user;
-    const { accessToken } = stsTokenManager;
-    const data = await createUser(firstName, lastName, email, uid);
-    dispatch(authenticate({ token: accessToken, userData: data }));
-  } catch (error) {
-    if (error.code === "auth/email-already-in-use") {
-      setError("The email address is already in use.");
-    } else if (error.code === "auth/weak-password") {
-      setError("The password is too weak.");
-    } else {
-      setError("An error occurred. Please try again later.");
-    }
-  }
+
+  const res = await createUserWithEmailAndPassword(auth, email, password);
+  const { uid, stsTokenManager } = res.user;
+  const { accessToken } = stsTokenManager;
+  const data = await createUser(firstName, lastName, email, uid);
+  dispatch(authenticate({ token: accessToken, userData: data }));
 };
